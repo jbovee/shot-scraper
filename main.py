11d965f54e0130ff72dabcfeb595c0c1785c5adf
jbovee <Mitouch.b@gmail.com>
2018-02-01 16:54:10 -0500

    Set up basics of getting team names and links

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup as bs, SoupStrainer as ss
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import lxml
 import sys
@@ -7,15 +7,28 @@ import re
 import json
 
 def main():
-	teams = get_teams()
-	for team,link in teams:
-		get_team_stats(team, link)
+    teams = get_teams()
+    '''
+    for team,link in teams:
+            get_team_stats(team, link)
+    '''
 
 def get_teams():
-	#return list of each team and link to their page on ESPN
+    #return list of each team and link to their page on ESPN
+    teamsPage = 'http://www.espn.com/mens-college-basketball/teams'
+    allTeams = requests.get(teamsPage)
+    allTeamsLinks = {}
+    teamLinkPattern = re.compile(r'mens-college-basketball/team/_/id/[0-9]+/[a-z-]+')
+    for link in BeautifulSoup(allTeams.text, 'lxml', parse_only=SoupStrainer('a', href=True)).find_all('a'):
+        if teamLinkPattern.search(link.get('href')):
+            allTeamsLinks[link.text] = link.get('href')
+    print(allTeamsLinks)
+
+    teamLinkEx = "http://www.espn.com/mens-college-basketball/team/_/id/399/albany-great-danes"
 
 def get_team_stats(teamName, teamLink):
-	#create JSON file for given team containing stats on shots for each player
+    #create JSON file for given team containing stats on shots for each player
+    return "blank"
 
 if __name__ == "__main__":
-	main()
+    main()
