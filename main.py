f14f874094183d015c08f4d2df530f7071c5128f
jbovee <Mitouch.b@gmail.com>
2018-02-03 00:21:33 -0500

    Modify parsed link to go to team schedule directly, to more easily obtain game links

--- main.py
+++ main.py
@@ -24,12 +24,20 @@ def get_teams():
 		for link in divisionLinksDiv.find_all('a'):
 			if teamLinkPattern.search(link.get('href')):
 				allTeamsLinks[divisionName][link.text] = {}
-				allTeamsLinks[divisionName][link.text]["teamLink"] = link.get('href')
+				allTeamsLinks[divisionName][link.text]["teamScheduleLink"] = 'schedule/_'.join(link.get('href').split('_'))
 	return allTeamsLinks
 
 def get_team_stats(teamName, teamLink):
 	#create JSON file for given team containing stats on shots for each player
-	teamLinkEx = "http://www.espn.com/mens-college-basketball/team/_/id/399/albany-great-danes"
+	#return internal JSON for team
+
+	#some games/teams don't have the nice little court map
+	#check for those and instead use just the timeline to extract shot info
+	#wont have court location info
+	teamEx = "http://www.espn.com/mens-college-basketball/team/_/id/399/albany-great-danes"
+	scheduleEx = "http://www.espn.com/mens-college-basketball/team/schedule/_/id/399"
+	gameEx = "http://www.espn.com/ncb/recap/_/gameId/400989985"
+	gameLinkPattern = re.compile(r'mens-college-basketball/game?gameId=[0-9]+')
 	return "blank"
 
 if __name__ == "__main__":
