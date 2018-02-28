009e2940a1e7f89b1b0a6db38066fc622abfed73
jbovee <Mitouch.b@gmail.com>
2018-02-28 11:05:55 -0500

    Get string from tuple returned by process.extractOne

--- main.py
+++ main.py
@@ -274,18 +274,18 @@ def parse_pbp(pbp,homeTeam,awayTeam):
 					shotIndex = homeShotIndex
 					score = int(scores.split('-')[1].strip())
 					if assistedName:
-						assistedName = process.extractOne(assistedName, homeTeam.keys(), scorer=fuzz.ratio)
+						assistedName = process.extractOne(assistedName, homeTeam.keys(), scorer=fuzz.ratio)[0]
 						assistedId = homeTeam[assistedName]
-					shooter = process.extractOne(shooter, homeTeam.keys(), scorer=fuzz.ratio)
+					shooter = process.extractOne(shooter, homeTeam.keys(), scorer=fuzz.ratio)[0]
 					homePbpShots.append((homeTeam[shooter],shooter,assistedId,assistedName,period+1,minutes,seconds,shotType,homeShotIndex,made,score))
 					homeShotIndex += 1
 				elif shooter in awayTeam:
 					shotIndex = awayShotIndex
 					score = int(scores.split('-')[0].strip())
 					if assistedName:
-						assistedName = process.extractOne(assistedName, awayTeam.keys(), scorer=fuzz.ratio)
+						assistedName = process.extractOne(assistedName, awayTeam.keys(), scorer=fuzz.ratio)[0]
 						assistedId = awayTeam[assistedName]
-					shooter = process.extractOne(shooter, awayTeam.keys(), scorer=fuzz.ratio)
+					shooter = process.extractOne(shooter, awayTeam.keys(), scorer=fuzz.ratio)[0]
 					awayPbpShots.append((awayTeam[shooter],shooter,assistedId,assistedName,period+1,minutes,seconds,shotType,awayShotIndex,made,score))
 					awayShotIndex += 1
 
